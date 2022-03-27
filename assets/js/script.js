06ae54fa3bc79a18a40f8973c969b3d1a7f05227
Ayva Valenti <94766574+ayvavalenti@users.noreply.github.com>
2022-03-27 12:45:48 -0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,50 @@
+//all variables being used
+
+var past;
+var present;
+var future;
+var timeT;
+var dateT;
+var input;
+var timeM;
+
+//doing the day
+
+function displayDate (){
+    console.log(displayDate);
+    document.getElementById("currentDay").innerHTML = 
+    Date (); 
+}
+dateT.setFullYear();
+dateT.setMonth();
+dateT.setDate();
+dateT.setDay();
+
+dateT = new Date();
+
+dateT;
+
+function saveBtn (){
+    document.getElementsByClassName("saveBtn").innerHTML =
+    localStorage ();
+}
+
+// doing the hours
+function displayTime () {
+ timeT = Date.now().getHours();
+}
+
+if (hour) {
+
+    if (timeT === timeM){
+
+    }
+
+}
+
+function setColor(element, color) {
+    element.style.backgroundColor = color;
+}
 $("#task-form .saveBtn").click(function() {
     var text = $("")
 });
